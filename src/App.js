5fa8dd4687bdbb4b72a91fbf37618019fd3d6f1b
chao <971064906@qq.com>
2019-06-11 00:18:44 +0800

    Add router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux'
 import { createStore, combineReducers } from 'redux';
 import { TodoApp } from './pages/TodoApp';
 import { todoApp } from './pages/reducer';
+import { HashRouter, Route, Link, Redirect } from "react-router-dom";
+import { Home } from './pages/Home'
+import { About } from './pages/About'
 
 const state = combineReducers({ todos: todoApp })
 const store = createStore(state, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
@@ -11,7 +14,31 @@ function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <TodoApp></TodoApp>
+        <HashRouter>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/home/user">User</Link>
+              </li>
+              <li>
+                <Link to="/home/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/todo">Todo</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route path="/" exact render={() => <Redirect to="/home"></Redirect>}></Route>
+          <Route path="/home" component={Home} ></Route>
+          <Route path="/todo" component={TodoApp} ></Route>
+          <Route path="/about" exact component={About}></Route>
+        </HashRouter>
       </Provider>
     </div>
   );
